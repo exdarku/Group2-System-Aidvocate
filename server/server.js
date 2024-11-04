62e14f6f88ff13b59caacd339ed0863b7a329d6e
Laurence Lesmoras <llesmoras@proton.me>
2024-11-04 19:42:45 +0800

    feat: added login endpoint

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const app = express();
 // Middleware for our backend/frontend
 app.use(express.json());
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 
 const indexRoutes = require('./routes/index');
+const loginRoutes = require('./routes/login');
 
 app.use('/', indexRoutes);
+app.use('/login', loginRoutes);
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
