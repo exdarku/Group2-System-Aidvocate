5dcd326494ecdc23718c187701c2c1c82e3e196b
Laurence Lesmoras <llesmoras@proton.me>
2024-11-04 22:08:29 +0800

    feat: added register ui at /register

---
+++ public/register/script.js
@@ -0,0 +1,26 @@
+document.getElementById('registerButton').addEventListener('click', async (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+
+    try {
+        const response = await fetch('http://localhost:3000/auth/register', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({username, password, firstName, lastName})
+        })
+
+        const data = await response.json();
+        if (response.ok){
+            alert("User registered successfully");
+        } else {
+            alert("There has been any error. Please try again later.");
+        }
+    } catch (error) {
+        alert("Error: " + error);
+    }
+})
